c08b2cf633dec1a5e4b53cf696643b706e365edb
Yovanis Bossio <yovanis2001@gmail.com>
2023-09-30 01:09:35 -0500

    P1-Pasos-React

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Importamnos las librerias correspondientes para inicar el proyecto
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+//creamos una variable donde vamos a renderizar el proyecto 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<>
+ <h1>Hola Mundo desde React</h1>
+</>);
